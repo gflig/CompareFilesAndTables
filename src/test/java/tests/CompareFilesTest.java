7087bd99d1534f966725777c7a5ca86b959dfa5e
Guy <g_flig@yahoo.com>
2022-09-07 21:01:57 -0400

    Changed file location in path

--- src/test/java/tests/CompareFilesTest.java
+++ src/test/java/tests/CompareFilesTest.java
@@ -9,11 +9,11 @@ public class CompareFilesTest {
     @Test
     public void compareFiles() throws IOException {
         //Set the paths of the files to compare
-//        String filePath1 = "C:\\Users\\g_fli\\QA\\CompareCSVFiles\\TestCSV_1.csv";
-//        String filePath2 = "C:\\Users\\g_fli\\QA\\CompareCSVFiles\\TestCSV_2.csv";
+        String filePath1 = "C:\\Users\\g_fli\\QA\\CompareFilesAndTables\\Sales_Records_10000_1.csv";
+        String filePath2 = "C:\\Users\\g_fli\\QA\\CompareFilesAndTables\\Sales_Records_10000_2.csv";
 
-        String filePath1 = "Sales_Records_10000_1.csv";
-        String filePath2 = "Sales_Records_10000_2.csv";
+//        String filePath1 = "Sales_Records_10000_1.csv";
+//        String filePath2 = "Sales_Records_10000_2.csv";
 
         CompareFiles compareFiles = new CompareFiles();
         compareFiles.compareTwoFiles(filePath1, filePath2);
